91c1dda4f962922bc0a101e04c2cf47f4d5aa27d
nextcam <guilherme.c.vogt@gmail.com>
2019-03-12 23:39:35 -0300

    Gradient strategy added

--- Least_squares/least_squares_linear.py
+++ Least_squares/least_squares_linear.py
@@ -1,19 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
+
 plt.rcParams['figure.figsize'] = (10.0, 8.0)
 
 def collect_data():
-    from sklearn import datasets, linear_model
-    diabetes = datasets.load_diabetes()
-    bmi_data_x = diabetes.data[:, np.newaxis, 2]
-    desease_y = diabetes.target[:]
-
-    # positive shift data
-    # x_min = np.min(bmi_data_x)
-    # bmi_data_x = bmi_data_x + (x_min * -1)
-
-    return bmi_data_x, desease_y
+    data = pd.read_csv('data.csv')
+    X = data.iloc[:, 0]
+    Y = data.iloc[:, 1]
+    return X, Y
 
 def quadratic_loss(predict, data_y):
 
@@ -23,6 +19,9 @@ def quadratic_loss(predict, data_y):
         loss += (data_y[i] - predict[i])**2
 
     return loss    
+
+def rmse(predict, data_y):
+    return np.sqrt(quadratic_loss(predict, data_y)/len(predict)) 
 
 def least_squares(data_x, data_y):
 
@@ -41,7 +40,23 @@ def least_squares(data_x, data_y):
     m = m_num / m_den
     c = mean_y - m*mean_x
 
-    return m, c    
+    return m, c
+
+def least_squares_grad(data_x, data_y, lr, iters, m = 0, c = 0):
+
+    N = len(data_x)
+
+    m_current = m
+    c_current = c
+
+    for i in range(iters):
+        y_current = (m_current * data_x) + c_current
+        m_gradient = -(2/N) * sum(data_x * (data_y - y_current))
+        c_gradient = -(2/N) * sum(data_y - y_current)
+        m_current = m_current - (lr * m_gradient)
+        c_current = c_current - (lr * c_gradient)
+
+    return m_current, c_current    
 
 if __name__ == "__main__":
 
@@ -56,6 +71,20 @@ if __name__ == "__main__":
     pred = m*x + c
 
     plt.plot(x, pred, color = 'red')
+
+    print("Loss: ", float(quadratic_loss(pred, y)))
+    print("RMSE: ", float(rmse(pred, y)))
+
+    m_grad, c_grad = least_squares_grad(x, y, 0.0001, 1000)
+
+    pred_grad = m_grad*x + c_grad
+
+    plt.plot(x, pred_grad, color = 'green')
+
+    plt.title('Loss LS_LR: {} \n RMSE LS_LR: {} \n Loss GLS_LR: {} \n RMSE GLS_LR: {}'\
+        .format(float(quadratic_loss(pred, y)), float(rmse(pred, y)), float(quadratic_loss(pred_grad, y)), float(rmse(pred_grad, y))))
+
     plt.show()
 
-    print(quadratic_loss(pred, y))
+    print("Loss: ", float(quadratic_loss(pred, y)))
+    print("RMSE: ", float(rmse(pred, y)))
