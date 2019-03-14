aaa650f1e403964457038fc025903ef20f18c585
guilhermecvogt <guilherme.c.vogt@gmail.com>
2019-03-13 22:50:20 -0300

    example_in

--- Least_squares/trabalho_1.m
+++ Least_squares/trabalho_1.m
@@ -11,3 +11,56 @@ y  = y2;               % sinal de saída   npts x
 Y   = [ ];  
 fi   = [ ];
 ordem = 2;
+
+for j = 1:npts
+
+if j<=2
+
+y1=y(2);  y2=y(1);  u1=u(2);  u2=u(1);
+
+else
+
+y1=y(j-1);  y2=y(j-2);  u1=u(j-1);  u2=u(j-2);
+
+end
+
+Y  = [Y; y(j)];
+
+fi = [fi;-y1 -y2 u1 u2];
+
+end
+
+theta = inv(fi'*fi)*fi'*Y;
+
+for i=1:size(theta,1)
+
+fprintf('theta(%d) = %f\n',i, theta(i));
+
+end
+
+for t=1:ordem
+
+yest(t) = y(t);     
+yest2(t) = y(t);
+
+end
+
+a1 = theta(1); a2 = theta(2);  
+
+b1 = theta(3); b2 = theta(4);
+
+for t = ordem+1 : npts
+
+yest(t) = -a1*yest(t-1)-a2*yest(t-2) + b1*u(t-1)+b2*u(t-2);
+
+yest2(t) = -a1*y(t-1)-a2*y(t-2) + b1*u(t-1)+b2*u(t-2);
+
+end
+
+plot(y,'g');        hold on;
+
+plot(yest,'r');      plot(yest2,'b'); 
+
+ylabel ('saída');    xlabel ('amostra');
+
+legend('real','prevista n passos à frente','prevista um passo à frente');
