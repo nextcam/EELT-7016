97063e5afec4cb9eba7b4a0766ef5d7b9b259a6f
Matheus Batista <matheusrenan.95@gmail.com>
2019-03-21 08:16:43 -0300

    adicao comentarios no code

--- Least_squares/trabalho_1a.m
+++ Least_squares/trabalho_1a.m
@@ -1,20 +1,22 @@
-%%%%%%%%%%%%%%%% BRA�O ROB�TICO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+%%%%%%%%%%%%%%%% BRACO ROBOTICO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
 
 clear all;  clc; close all;
 
-fprintf('M�todo dos m�nimos quadrados em batelada\n\n');
+fprintf('Metodo dos minimos quadrados em batelada\n\n');
 
-load robot_arm.dat; %dados - ftp://ftp.esat.kuleuven.be/pub/SISTA/data/mechanical/robot_arm.txt 
+% Inicializacao dos dados de entrada e saida
 
-npts = size(robot_arm,1); % n�mero de amostras a considera
+load robot_arm.dat; % dados encontrados em ftp://ftp.esat.kuleuven.be/pub/SISTA/data/mechanical/robot_arm.txt 
 
-u_in = robot_arm(:,1);
-y_in = robot_arm(:,2);
-u = u_in;
-y = y_in;
-Y   = [ ];  
-fi   = [ ];
-ordem = 2;
+npts = size(robot_arm,1); % numero de entradas, que representa o numero de amostras
+
+u = robot_arm(:,1); % colocando a primeira coluna dos dados (entrada) na variavel u
+y = robot_arm(:,2); % colocando a segunda coluna (saida) na variavel y
+Y = [ ]; % vetor das saidas inicialmente vazio
+fi = [ ]; % matriz de observacao
+ordem = 2; % sistema de segunda ordem
+
+% Encontrando os polos e zeros
 
 % for j = 1:npts
 % 
@@ -33,7 +35,6 @@ ordem = 2;
 % 
 % end
 % 
-% %THE BOY
 % theta = inv(fi'*fi)*fi'*Y;
 % 
 % for i=1:size(theta,1)
@@ -76,25 +77,28 @@ ordem = 2;
 % 
 % legend('real','prevista n passos � frente','prevista um passo � frente');
 
-y_train = y(1:round(size(y,1)*0.7));
-y_test = y(round(size(y,1)*0.7): size(y,1));
 
-u_train = u(1:round(size(u,1)*0.7));
-u_test = u(round(size(u,1)*0.7): size(u,1));
 
-DAT_train = iddata(y_train, u_train);
-DAT_test = iddata(y_test, u_test);
+y_train = y(1:round(size(y,1)*0.7)); % Reservando 70% da saida para treinamento
+y_test = y(round(size(y,1)*0.7) + 1: size(y,1)); % Reservando o restante para teste
 
-systems = [];
-best_mse = 1;
+u_train = u(1:round(size(u,1)*0.7)); % Reservando 70% da entrada para treinamento
+u_test = u(round(size(u,1)*0.7) + 1: size(u,1)); % Reservando o restante para teste
+
+DAT_train = iddata(y_train, u_train); % Cria um objeto 'iddata' para poder usar na funcao arx e armax
+DAT_test = iddata(y_test, u_test); % Cria um objeto 'iddata' para poder usar na funcao 'sim' e estimar a saida
+systems = []; % Vetor 'systems' para adicionar todos os sistemas treinados
+best_mse = 1; % Para comparar com o MSE do primeiro sistema
+
+fprintf('--------------------ARX--------------------\n\n');
 
 for a=1:3
     for b=1:3
         sys = arx(DAT_train, [a b 0]); % Estimate over train set
-        %systems = [systems ; sys];
+        systems = [systems; sys];
         y_est = sim(sys, DAT_test);
         MSE = goodnessOfFit(y_test, y_est.y,'MSE');
-        fprintf("Sistema ARX - P�los = %d; Zeros = %d | MSE = %0.2f %% \n", a, b, MSE*100);
+        fprintf('Sistema ARX - Polos = %d; Zeros = %d | MSE = %0.2f %% \n', a, b, MSE*100);
         
         if MSE < best_mse
             best_mse = MSE;
@@ -106,17 +110,17 @@ for a=1:3
     fprintf('\n');
 end
 
-fprintf("----------------------------------------------------\n\n");
+fprintf('-------------------ARMAX-------------------\n\n');
 
 sys = [];
 
 for a=1:3
     for b=1:3
         sys = armax(DAT_train, [a b 3 0]); % Estimate over train set
-        %systems = [systems ; sys];
+        systems = [systems; sys];
         y_est = sim(sys, DAT_test);
         MSE = goodnessOfFit(y_test, y_est.y,'MSE');
-        fprintf("Sistema ARMAX - P�los = %d; Zeros = %d | MSE = %0.2f %% \n", a, b, MSE*100);
+        fprintf('Sistema ARMAX - Polos = %d; Zeros = %d | MSE = %0.2f %% \n', a, b, MSE*100);
         
         if MSE < best_mse
             best_mse = MSE;
@@ -125,15 +129,17 @@ for a=1:3
             best_sys = sys;
         end
     end
-    fprintf("\n");
+    fprintf('\n');
 end
 
- fprintf("Best Error =  %0.2f %% | P�los = %d; Zeros = %d \n", best_mse*100, best_a, best_b);
- fprintf("\nBest System:\n");
- best_sys
+fprintf('-------------------------------------------\n\n');
+
+fprintf('Best Error =  %0.2f %% | Polos = %d; Zeros = %d \n', best_mse*100, best_a, best_b);
+fprintf('\nBest System:\n');
+best_sys
 
 %figure();
-%compare(DAT_test, best_sys, 1);
+%compare(DAT_test, best_sys, 1);s
 best_y_est = sim(best_sys, DAT_test);
 
 [h_lillie, p_lillie] = lillietest(best_y_est.y)
